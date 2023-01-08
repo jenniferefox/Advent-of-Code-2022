6884fc57d43fada7c3325e7ddc84d4c2d10b816b
jenniferefox <47497617+jenniferefox@users.noreply.github.com>
2023-01-08 17:27:35 +0000

    Day 4 Part 2 Solution

---
+++ AdventofCode2022_day4part2.py
@@ -0,0 +1,17 @@
+import re
+
+with open("input_day4.txt") as file:
+    counter = 0
+    for line in file:
+        bounds = [int(s) for s in re.findall('\d+', line)]
+        if bounds[1] >= bounds[2] and bounds[0] <= bounds[2]:
+            counter += 1
+        elif bounds[3] >= bounds[0] and bounds[2] <= bounds[0]:
+            counter += 1
+        elif bounds[0] <= bounds[2] and bounds[3] <= bounds[1]:
+            counter += 1
+        elif bounds[2] <= bounds[0] and bounds[1] <= bounds[3]:
+            counter += 1
+            #print(bounds)
+
+    print(counter)
